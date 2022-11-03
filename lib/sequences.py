fbe3a5553d8b6d48a3584a55b4b86c5fbcf4e625
Ben Botsford <ben.botsford@flatironschool.com>
2022-11-03 10:04:52 -0500

    re-add solution

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
 
+# With range:
+
 def print_fibonacci(length):
     pass
+    fibonacci_sequence = []
+    if length > 0:
+        fibonacci_sequence.append(0)
+        if length > 1:
+            fibonacci_sequence.append(1)
+            if length > 2:
+                for i in range(2, length):
+                    fibonacci_sequence.append(
+                        fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+
+    print(fibonacci_sequence)
+
+# Without range:
+
+# def print_fibonacci(length):
+#     fibonacci_sequence = []
+#     if length > 0:
+#         fibonacci_sequence.append(0)
+#         if length > 1:
+#             fibonacci_sequence.append(1)
+#             if length > 2:
+#                 i = 2
+#                 while i < length:
+#                     fibonacci_sequence.append(
+#                         fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+#                     i += 1
+
+#     print(fibonacci_sequence)
