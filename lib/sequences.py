9786a45ffec9df011aa5c5c7862588848d60a0e6
sean <balayans2014@gmail.com>
2023-07-21 15:09:19 -1000

    fixes solution code because it does not match requirements of the test

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,47 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+	pass
+	fib_seq = []
+	if length > 0:
+		fib_seq.append(0)
+	if length >= 2:
+		fib_seq.append(1)
+		for i in range(2, length):
+			fib_seq.append(fib_seq[-1] + fib_seq[-2])
+
+	print(fib_seq)
+
+# With range **OLD SOLUTION**:
+
+# def print_fibonacci(length):
+#     pass
+#     fibonacci_sequence = []
+#     if length > 0:
+#         fibonacci_sequence.append(0) 
+#         if length > 1:
+#             fibonacci_sequence.append(1)
+#             if length > 2:
+#                 for i in range(2, length):
+#                     fibonacci_sequence.append(
+#                         fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+
+#     print(fibonacci_sequence)
+
+
+# Without range:
+
+# def print_fibonacci(length):
+#     fibonacci_sequence = []
+#     if length > 0:
+#         fibonacci_sequence.append(0)
+#         if length > 1:
+#             fibonacci_sequence.append(1)
+#             if length > 2:
+#                 i = 2
+#                 while i < length:
+#                     fibonacci_sequence.append(
+#                         fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+#                     i += 1
+
+#     print(fibonacci_sequence)
